4dfaa511c944c1a10da6682ba7f9d8bb9fb5cc21
Helgi <helgimagic@yandex.ru>
2024-02-22 20:06:54 +0300

    task 15 started

--- src/components/ui/Button/Button.styled.js
+++ src/components/ui/Button/Button.styled.js
@@ -1,4 +1,19 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const COLOR = {
+  green: css`
+    color: #fff;
+    background: #6e807a;
+
+    &:hover {
+      background: 899c95;
+    }
+  `,
+  secondary: css`
+    color: #000;
+    background: linear-gradient(#c7c7d2, #bcbaba);
+  `,
+};
 
 const MyButton = styled.button`
   padding: 10px 0;
